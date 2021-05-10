22a81033fd7b024b3767e2632fbf683852977103
ScottAgirs <scott@ijs.to>
2021-05-10 13:34:30 -0400

    chore: [minor] Update editor
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- components/SlateEditor.js
+++ components/SlateEditor.js
@@ -1,21 +1,23 @@
 // Import React dependencies.
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useState } from 'react';
 // Import the Slate editor factory.
-import { createEditor } from 'slate'
+import { createEditor } from 'slate';
 
 // Import the Slate components and React plugin.
-import { Slate, Editable, withReact } from 'slate-react'
+import { Slate, Editable, withReact } from 'slate-react';
 
+const initialValue = [
+  {
+    children: [{ text: 'This text is coming from a Slate node.' }],
+    type: 'paragraph',
+  },
+];
 
 const SlateEditor = () => {
-  const editor = useMemo(() => withReact(createEditor()), [])
-  // Add the initial value when setting up our state.
-  const [value, setValue] = useState([
-    {
-      type: 'paragraph',
-      children: [{ text: 'A line of text in a paragraph.' }],
-    },
-  ])
+  // https://github.com/ianstormtaylor/slate/issues/3477#issuecomment-805901596
+  const [editor] = useState(() => withReact(createEditor()));
+  // Add the initial value when setting up our state
+  const [value, setValue] = useState(initialValue);
 
   return (
     <Slate
@@ -25,7 +27,7 @@ const SlateEditor = () => {
     >
       <Editable />
     </Slate>
-  )
-}
+  );
+};
 
 export default SlateEditor;
